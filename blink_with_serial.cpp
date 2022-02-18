0bf4dcdfe425a3002a9e5d2d84eb3c3aa822a5b9
segin <98380527+seginbytes@users.noreply.github.com>
2022-02-19 00:28:54 +0530

    Create blink_with_serial.cpp

---
+++ blink_with_serial.cpp
@@ -0,0 +1,18 @@
+int LED=13;
+int dt=500;
+
+void setup ()
+{
+  pinMode(LED,OUTPUT);
+  Serial.begin(9600);
+}
+void loop ()
+{
+  digitalWrite(LED,HIGH);
+  Serial.println("LED ON");
+  delay(dt);
+  digitalWrite(LED,LOW);
+  Serial.println("LED OFF");
+  delay(dt);
+
+}
