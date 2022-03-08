5a06e35f5b00fd2705115372aaf268b7a549c0d5
segin <98380527+seginbytes@users.noreply.github.com>
2022-03-08 22:23:09 +0530

    Update milies as delay.cpp

--- milies as delay.cpp
+++ milies as delay.cpp
@@ -1,1 +1,40 @@
 
+# include<Arduino.h>
+
+const int redled = 2;
+const long interval = 100;
+int ledstate = LOW;
+unsigned long prevtime = 0;
+
+void setup()
+{
+  pinMode(redled,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop ()
+{
+  unsigned long currtime = millis();
+
+  if (currtime - prevtime >= interval)
+  {
+    prevtime = currtime;
+    Serial.print("curr:  ");
+     Serial.print(currtime);
+      Serial.print("   prev:  ");
+       Serial.println(prevtime);
+
+    if (ledstate == LOW) 
+    {
+      ledstate = HIGH;
+    } 
+    else 
+    {
+      ledstate = LOW;
+    }
+    digitalWrite(redled,ledstate);
+  }
+}
+
+
+
