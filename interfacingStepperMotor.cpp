8afa399805b403e7bd182c212f9240d0aa815db3
segin <98380527+SEGIN-zz@users.noreply.github.com>
2022-03-24 22:28:55 +0530

    Create interfacingStepperMotor.cpp

---
+++ interfacingStepperMotor.cpp
@@ -0,0 +1,21 @@
+# include <Arduino.h>
+# include <Stepper.h>
+
+int spr = 2048;
+int mot_speed = 10;
+int dt = 500;
+Stepper Stepper_zero (spr,8,9,10,11);
+
+void setup()
+{
+  Serial.begin(9600);
+  Stepper_zero.setSpeed(mot_speed);
+}
+
+void loop ()
+{
+  Stepper_zero.step(spr);
+  delay(dt);
+  Stepper_zero.step(-spr);
+  delay(dt);
+}
