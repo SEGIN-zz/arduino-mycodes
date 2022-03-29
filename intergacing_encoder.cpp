c71f2dce1d4672a787e4f0716e2068712269cc6d
seginbytes <segin.bytes@gmail.com>
2022-03-29 18:00:29 +0530

    .

--- intergacing_encoder.cpp
+++ intergacing_encoder.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+int clk = 2;
+int clkVal;
+int dt = 3;
+int dtVal;
+
+void setup()
+{
+  Serial.begin(9600); // setting up SerialMonitor with a baudrate of 9600;
+  pinMode(clk,INPUT);
+  pinMode(dt,INPUT);
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  clkVal = digitalRead(clk); // reading the value of the encoder output pin clk,dt;
+  dtVal = digitalRead(dt);
+  
+  Serial.print("clk = ");
+  Serial.print(clkVal);
+  Serial.print("  Dt = ");
+  Serial.println(dtVal);
+
 }
