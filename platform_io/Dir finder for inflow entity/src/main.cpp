da8a0c12254fe9eeba7e2426b45b7afa35852eef
segin-GH <segin.bytes@gmail.com>
2022-07-28 12:11:59 +0530

    .

--- platform_io/Dir finder for inflow entity/src/main.cpp
+++ platform_io/Dir finder for inflow entity/src/main.cpp
@@ -4,54 +4,78 @@
 Write the code for the selected sensor to print the direction detected.
 (Use any MC: NodeMCU, RPi, UNO, Nano, Mega, ESP, MSP)(Entity - consider a bowling system where the ball is your entity)
 
-Ans) 
+Ans : considering a bowling system  a ball that goes through a channel therefore, 
+i will be using object detection ,using ldr and led for object detection 
+there will be a array of led and ldr equaly spaced this spaing should be 
+propotional to the size & speed of the entitiy also its logic is similar to 
+a rotary encoder this way it is cheap, and effective;
 
-considering a bowling system  a ball that goes through a channel 
+using arduino uno;
+using platform.io 
 
-therefore, i will be using object detection ,using ldr and led for object detection
 
 */
+ # include <Arduino.h>        
+// this  is for including arduino frameWork in platform.io
 
-
-
- # include <Arduino.h>
-
-
-
-class Detector
+// using oop
+class Detector                 
 {
-  
-  private:
-
-    int _ledPin;
+// pair of a led and  ldr together form this object called Detector
+// this object stores & gets data
+ 
+  private: 
+// init pins for led & ldr
+    int _ledPin;                
     int _ldrPin;
     int _ldrVal;
-    int _detectioin_status;
-
+// used to store ldr val
+    int _detectioin_status;    
+// used to store detecion status
+  
   public:
-
-
-    void init_detector(int ledPin, int ldrPin)          // inti the detector
+/*
+ init_detector is used  intizing  the detector,  
+ this function takes two arg, pinNumber of led and ldr
+ by this we can init how many detectors that we want;
+*/
+    void init_detector(int ledPin, int ldrPin)          
     {
       _ledPin = ledPin;
       _ldrPin = ldrPin;
-      pinMode(_ledPin,OUTPUT);
+      pinMode(_ledPin,OUTPUT);                          
       pinMode(_ldrPin,INPUT);
-      Serial.println("setupComplete ...");
+      // Serial.println("setupComplete ...");               // just  4 debuging
     }
-
-    void start_detection()
+/*
+stat_detecion is used to stat the detector it turns on the
+led and reads the ldr pin and prints the ldr val 
+led turns on and the ldr recives the light 
+*/
+    void start_detection()                            
     {
       digitalWrite(_ledPin,HIGH);
       _ldrVal = analogRead(_ldrPin);
       Serial.println(_ldrVal);
       Serial.println("detection started");
     }
+/* 
+get_detectionStatus is used for getting status of the current condition
+LDR is a component that has aresistance that changes with the light intensity that falls upon it. 
+thus when the led light falls upon it we can assume that there is no objet there
+when the light of led doesnot fall upon ldr we can asume that the object is detected
+in this case a bowling ball
+*/
 
-    int get_detectionStatus()                         // to get status
+    int get_detectionStatus()                         
     {
       _ldrVal = analogRead(_ldrPin);
-      if (_ldrVal >= 1000)
+// reading the val of ldr
+/* 
+logic for the ball detection if the ball is detected
+it returns a 1, if not detected return 0;
+*/
+      if (_ldrVal >= 1000) // 1000 is a asumed val
       {
          _detectioin_status = 1;          
       }
@@ -60,49 +84,74 @@ class Detector
          _detectioin_status = 0;
       }
       return _detectioin_status;
-      
-      Serial.println("detection approved");
     }
 
 };
-
+/*
+constructing 3 objects as detectors 
+*/
 Detector detectorOne, detectroTwo, detectorThree;
 
 void setup()
 {
-  Serial.begin(11500);
-
+  Serial.begin(11500);  // used to begin and set baudrate of a serial monitor;
+/*
+now we need to inti the detector, i will be using 3 detector combo 
+intitzing the objects it takes two input first led pin & second ldrPin
+*/
   detectorOne.init_detector(2,A0);
   detectroTwo.init_detector(3,A1);
   detectorThree.init_detector(5,A2);
 
+// now we need to start the detetion 
   detectorOne.start_detection();
   detectroTwo.start_detection();
-  detectorThree.start_detection();      // statring detection;
-
+  detectorThree.start_detection();      
 }
 
 void loop()
 {
 
-  // logic for ball detection 
+/* 
+logic for ball detection
+if the first detector satate is 1 and the rest of the detector state is low
+we can asume that the ball is mooving forward;
+
+**note
+detectors are kept in a order left to right 
+1 detector at the left side , 2 in middle and third at last
+if this orentaion is chagned you will need to change the direction ;
+*/
 
   if(detectorOne.get_detectionStatus() == 1 && detectroTwo.get_detectionStatus() == 0 
   && detectorThree.get_detectionStatus() == 0)
   {
 
     Serial.println("Forward");
+    // printing the direction
 
   }
+/* 
+if the third detector satate is 1 and the rest of the detector state is  0
+we can asume that the ball is mooving backwards;
+
+**note
+detectors are kept in a order left to right 
+1 detector at the left side to 2 in middle and third at last
+if this orentaion is chagned you will need to change the direction in this also;
+*/
 
   if (detectorThree.get_detectionStatus() == 1 && detectroTwo.get_detectionStatus() == 0 
   && detectorOne.get_detectionStatus() == 0)
   {
 
-
     Serial.println("Backward");
-
+    // printing the direction
+    
   }
+/*
+this will be working in a infinit loop for the ball detecion
+*/
 
 
 }
