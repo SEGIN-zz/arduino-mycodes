00f884cbf70fbf223b74aef77dd2ad5f8b229e25
segin-GH <segin.bytes@gmail.com>
2022-09-19 17:17:16 +0530

    .

--- ESP_FREE_RTOS/analogeRead_esp32_freeRtos/src/main.cpp
+++ ESP_FREE_RTOS/analogeRead_esp32_freeRtos/src/main.cpp
@@ -1,9 +1,41 @@
-#include <Arduino.h>
+# include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+# if ! CONFIG_FREERTOS_UNICORE
+# define TASK_CPU APP_CPU_NUM
+# else
+# define TASK_CPU PRO_CPU_NUM
+# endif
+
+int potVal = 0;
+
+
+void readpot(void *parm)
+{
+  while(true)
+  {
+    potVal = analogRead(35);
+    Serial.println(potVal);
+    vTaskDelay(100/portTICK_PERIOD_MS);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(35,INPUT);
+  vTaskDelay(1000/portTICK_PERIOD_MS);
+  xTaskCreatePinnedToCore(
+    readpot,
+    "readPot",
+    1024,
+    NULL,
+    1,
+    NULL,
+    APP_CPU_NUM
+  );
+}
+
+void loop()
+{
+  //
 }
