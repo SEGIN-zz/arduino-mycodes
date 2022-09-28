e7baddc659eccacbf28ae8c8268a890d7bc809e0
segin-GH <segin.bytes@gmail.com>
2022-09-28 16:23:22 +0530

    .

---
+++ arduino_codes/blinkWithSerial/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN,OUTPUT);
+}
+
+void loop()
+{
+  Serial.println("Led ON");
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN,LOW);
+  Serial.println("Led OFF");
+  delay(1000);
+
+}
