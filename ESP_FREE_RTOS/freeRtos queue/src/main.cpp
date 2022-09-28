239da77b41364dd8b015e8410eb36aaefb840bc6
segin-GH <segin.bytes@gmail.com>
2022-09-28 09:50:31 +0530

    .

--- ESP_FREE_RTOS/freeRtos queue/src/main.cpp
+++ ESP_FREE_RTOS/freeRtos queue/src/main.cpp
@@ -1,9 +1,60 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#if !config_unicore
+#define TASK_CPU APP_CPU_NUM
+#else
+#define TASK_CPU PRO_CPU_NUM
+#endif
+
+static const uint8_t queue_len = 5;
+static QueueHandle_t msg_queu;
+
+
+void printMsg(void *parm)
+{
+  int item;
+
+  while (true)
+  {
+    if (xQueueReceive(msg_queu,(void*)&item,0) == pdTRUE)
+    {
+      Serial.println(item); 
+    }
+    vTaskDelay(1000/portTICK_PERIOD_MS);
+
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  Serial.begin(115200);
+  vTaskDelay(1000/portTICK_PERIOD_MS);
+  Serial.println();
+  Serial.print("------Queue Demo------");
+  msg_queu = xQueueCreate(queue_len,sizeof(int));
+
+  xTaskCreatePinnedToCore(
+    printMsg,
+    "printMsg",
+    1024,
+    NULL,
+    1,
+    NULL,
+    APP_CPU_NUM
+  );
+
+}
+
+void loop()
+{
+  static int num = 0;
+
+  if (xQueueSend(msg_queu,(void*) &num,10) != pdTRUE)
+  {
+    Serial.println("Queue full");
+  }
+  num++;
+  vTaskDelay(500/portTICK_PERIOD_MS);
+
+
 }
